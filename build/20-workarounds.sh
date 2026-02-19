a67d0af632ceb5a75d298465e6ff21623b1ffcb5
Stephan Lüscher <code@luescher.io>
2026-02-19 22:23:43 +0100

    feat(package): configure waybar default settings

---
+++ build/20-workarounds.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eoux pipefail
+
+echo "::group:: Implement Workarounds"
+
+# Remove fuzzel default config, we have a custom one at /usr/share/flvos/etc/xdg/fuzzel
+rm -rf /etc/xdg/fuzzel
+
+# Remove waybar default config, we have a custom one at /usr/share/flvos/etc/xdg/waybar
+# XDG_CONFIG_DIRS not supported -> https://github.com/Alexays/Waybar/wiki/Configuration#config-file
+rm -rf /etc/xdg/waybar
+ln -sf /usr/share/flvos/etc/xdg/waybar /etc/xdg/waybar
+
+# Link custom scripts to /usr/bin/ since setting PATH via /usr/lib/environment.d/ is not working
+find /usr/share/flvos/bin -maxdepth 1 -type f -exec ln -sf {} /usr/bin/ \;
+
+echo "::endgroup::"
